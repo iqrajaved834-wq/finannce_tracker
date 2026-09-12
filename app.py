c4ebd49ff57eff31f5c6a78d428207c9a18eec89
iqrajaved834 <iqrajaved834@gmail.com>
2026-09-12 21:59:46 +0500

    project done

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def export_page():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 import os
-import MySQLdb.cursors
 from dotenv import load_dotenv
+
 load_dotenv()
- 
+
 class config:
-   
+
     MYSQL_HOST = os.getenv('DB_HOST')
     MYSQL_USER = os.getenv('DB_USER')
     MYSQL_PASSWORD = os.getenv('DB_PASSWORD')
     MYSQL_DB = os.getenv('DB_NAME')
     SECRET_KEY = os.getenv('SECRET_KEY')
-    MYSQL_CURSORCLASS = "DictCursor" 
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    MYSQL_CURSORCLASS = "DictCursor"
